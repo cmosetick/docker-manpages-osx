65717b4cfe68f693c22755df13aa3c495d8aebdc
Chris Mosetick <cmosetick@gmail.com>
2017-03-05 14:47:15 -0800

    misc. cleanup, minor update

--- docker-man-pages-osx.sh
+++ docker-man-pages-osx.sh
@@ -10,9 +10,9 @@ cd docker/man
 docker run --rm -v $PWD/:/docs:rw -w /docs -i --entrypoint=/docs/md2man-all.sh docker-man-pages-osx:latest
 cp -R man* /usr/local/share/man/   # no sudo for homebrew location
 rm -rf $HOME/temp/docker	   # cleanup
-ls -la /usr/local/share/man/man1 | grep docker
-ls -la /usr/local/share/man/man5 | grep docker
-ls -la /usr/local/share/man/man8 | grep docker
+ls -l /usr/local/share/man/man1/docker*
+ls -l /usr/local/share/man/man5/docker*
+ls -l /usr/local/share/man/man8/docker*
 echo "print out the known currently installed man pages"
 echo "note that some of these man pages do not apply to Docker for Mac / Docker Community Edition"
 echo "DISPLAY THREE COMMONLY USED DOCKER MAN PAGES"
