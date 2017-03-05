9928b4c1be8873add51a06e8fcee71fb838ab39f
Chris Mosetick <cmosetick@gmail.com>
2017-03-05 14:38:32 -0800

    update generation script to use new Dockerfile

--- docker-man-pages-osx.sh
+++ docker-man-pages-osx.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
-# adapted from: http://stackoverflow.com/a/32239112
+# originally adapted from: http://stackoverflow.com/a/32239112
 
-mkdir $HOME/temp
+docker build -t docker-man-pages-osx .
+mkdir -p $HOME/temp
 cd $HOME/temp
 git clone https://github.com/docker/docker.git
-cd docker/man                      # looks like the directory has moved up
-docker build -t docker/md2man .    # don't forget the '.'
-docker run -v $PWD/:/docs:rw -w /docs -i docker/md2man /docs/md2man-all.sh
+cd docker/man
+docker run --rm -v $PWD/:/docs:rw -w /docs -i --entrypoint=/docs/md2man-all.sh docker-man-pages-osx:latest
 cp -R man* /usr/local/share/man/   # no sudo for homebrew location
 rm -rf $HOME/temp/docker	   # cleanup
-man docker                         # check it worked on OS X
+ls -la /usr/local/share/man/man1 | grep docker
+ls -la /usr/local/share/man/man5 | grep docker
+ls -la /usr/local/share/man/man8 | grep docker
+echo "print out the known currently installed man pages"
+echo "note that some of these man pages do not apply to Docker for Mac / Docker Community Edition"
+echo "DISPLAY THREE COMMONLY USED DOCKER MAN PAGES"
+man docker && man docker-exec && man docker-volume
