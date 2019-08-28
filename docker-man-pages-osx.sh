daaeb57e2e70e670f71401c380740f8be7422f6a
Chris Mosetick <chris.mosetick@arcteryx.com>
2019-08-28 10:22:23 -0700

    update directory cleanup

--- docker-man-pages-osx.sh
+++ docker-man-pages-osx.sh
@@ -13,7 +13,7 @@ cd moby/man
 docker run --rm -v $PWD/:/man:rw -w /man -i --entrypoint=/man/md2man-all.sh docker-man-pages-osx:latest
 
 cp -R man* /usr/local/share/man/   # no sudo for homebrew location
-rm -rf $HOME/temp/docker-ce	   # cleanup
+rm -rf $HOME/temp/moby   # cleanup
 ls -l /usr/local/share/man/man1/docker*
 ls -l /usr/local/share/man/man5/docker*
 ls -l /usr/local/share/man/man8/docker*
